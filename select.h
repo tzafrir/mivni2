3faee0422960356d5c9a06cd7816e62f0c7040dd
ilya <ilya@ein-hashofet.co.il>
2009-06-17 06:27:03 +0200

    fix, select,
    improve partition code, i know i need to care only about asimptotoic time
    but i cant :(

--- select.h
+++ select.h
@@ -2,25 +2,38 @@
 #define _SELECT_H
 
 #include <stdlib.h>
-#include <time.h>
 #include <iostream>
+#include <cassert>
 
 using std::cout;
 
 class Select {
 	//A must have more then two element
-	static int partition(int* A, int first, int last, int index) 
+	static int partition(int* A, int Size, int index) 
 	{
-		swap(A[first],A[index]);
-		int* Smaller=A+first;
-		int* Larger=A+last+1;
+		int pivot = A[index];
+		
+		int* Smaller=A;
+
+		while (*(Smaller) < pivot) Smaller++; //will stop at pivot in worst case
+		swap(*Smaller,A[index]);
+
+		index = Smaller - A;
+		if (index == Size-1)
+		{
+			return index;
+		}
+
+		int* Larger=A+Size;
 
 		while (true)
 		{
-			while (*(--Larger) > A[first] && Larger > Smaller);
+			while (*(--Larger) > pivot); //we know the loop will stop in
+									    //A[index] in the worsk case
 		
-			while (*(++Smaller) < A[first] && Larger > Smaller);
-
+			while (*(++Smaller) < pivot);  //we know the loop will stop in
+									    //becuse we placed a >= item in A[index] in the
+										//the begging
 			if (Larger > Smaller)
 			{
 				swap(*Larger,*Smaller);
@@ -31,7 +44,7 @@ class Select {
 			}
 		} 
 
-		swap(A[first],*Larger);
+		swap(A[index],*Larger);
 		return Larger-A;
 	}
 
@@ -42,50 +55,61 @@ class Select {
 	}
 	
 	public:
-	static void select(int* A, int first, int last, int index) 
+
+	static void select(int* A,int Size, int index) 
 	{
-		int pivot;
-		if (last - first <= 9)
+		index--; //its we work with indexes 0-size-1 the index'th item in in positon 
+		//index -1
+		int last=Size-1, first=0;
+
+		while (true)
 		{
-			if (first == last)
+			assert(first <= last);
+			
+			int pivot;
+			if (last - first < 15) //median of medians isnt useful if we have 
+				//less then 3 groups of five...
+			{
+				if (first == last)
+				{
+					return;
+				}
+				pivot = last;
+			}
+			else
+			{
+				int medians= first;
+				for (int i = medians; i <= last- 4;	medians++, i+=5)
+				{
+					select(&A[i],5,3);
+					swap(A[i+2],A[medians++]);
+				}
+
+				pivot = (medians - first) /2;
+
+				select(&A[first],medians,pivot);
+			}
+			
+			pivot = partition(A,Size,pivot);
+
+
+			if (index < pivot)
 			{
 				return;
+				last = pivot-1;
 			}
-			pivot = last;
-		}
-		else
-		{
-			int medians= first;
-			for (int i = medians; i <= last- 4;	medians++, i+=5)
+			else
 			{
-				select(A,i,i+4,3);
-				swap(A[i+2],A[medians++]);
+				if (index == pivot) 
+				{
+					return;
+				}
+
+				first = pivot+1;
+				return;
 			}
-
-			pivot = (medians - first) /2;
-
-			select(A,first,medians-1,pivot);
-		}
-		
-		pivot = partition(A,first,last,pivot);
-
-		if (index-1 < pivot)
-		{
-			select(A,first,pivot-1,index);
 			
 		}
-		else
-		{
-			select(A,pivot,last,index);	
-		}
-		/*
-		std::cout << std::endl << index - first << std::endl;
-		for (int i = first; i <= last; i++)
-		{
-			std::cout << A[i] << ',' ;
-		}
-		std::cout << std::endl << pivot << std::endl;
-		*/
 	}
 		
 };
